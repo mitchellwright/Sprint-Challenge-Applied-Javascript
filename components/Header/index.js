514360b95ffe870aceec42463f41f5ae5b736324
Mitchell Wright <mitchellbwright@gmail.com>
2020-06-12 17:16:45 -0600

    complete Header component

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,27 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    let headerParent = document.createElement('div');
+    headerParent.classList.add('header');
+
+    let date = document.createElement('span');
+    date.classList.add('date');
+    date.textContent = 'March 28, 2019';
+    headerParent.appendChild(date);
+
+    let title = document.createElement('h1');
+    title.textContent = 'Lambda Times';
+    headerParent.appendChild(title);
+
+    let temperature = document.createElement('span');
+    temperature.classList.add('temp');
+    temperature.textContent = '98°';
+    headerParent.appendChild(temperature);
+
+    return headerParent;
+}
+
+const headerContainer = document.querySelector('.header-container');
+headerContainer.appendChild(Header());
+
